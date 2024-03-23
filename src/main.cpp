4cfd62161298aacfa6907646b44333dc4cdc0988
qart <thissak@gmail.com>
2024-03-23 21:08:20 +0900

    add OnKeyEvent

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,21 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+void OnKeyEvent(GLFWwindow* window, 
+    int key, int scancode, int action, int mods){
+        SPDLOG_INFO("key: {}, scancode: {}, mods: {}{}{}",
+        key, scancode, 
+        action == GLFW_PRESS ? "Pressed" :
+        action == GLFW_RELEASE ? "Release" :
+        action == GLFW_REPEAT ? "Repeat" : "Unknown",
+        mods & GLFW_MOD_CONTROL ? "C" : "-",
+        mods & GLFW_MOD_SHIFT ? "S" : "-",
+        mods & GLFW_MOD_ALT ? "A" : "-");
+        if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS){
+            glfwSetWindowShouldClose(window, true);
+        }
+    }
+
 void OnFramebufferSizeChange(GLFWwindow* window, int width, int height){
     SPDLOG_INFO("framebuffer size changed: ({} x {}", width, height);
     glViewport(0, 0, width, height);
@@ -44,7 +59,9 @@ int main(int arc, const char** argv){
     const char* glVersion = reinterpret_cast<const char*>(glGetString(GL_VERSION));
     SPDLOG_INFO("OpenGL context version: {}", glVersion);
 
+    OnFramebufferSizeChange(window, WINDOW_WIDTH, WINDOW_HEIGHT);
     glfwSetFramebufferSizeCallback(window, OnFramebufferSizeChange);
+    glfwSetKeyCallback(window, OnKeyEvent);
 
     // glfw 루프실행, 윈도우 CLOSE 버튼을 누르면 정상 종료
     SPDLOG_INFO("Start main loop");
