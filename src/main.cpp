454f63ff7b8612ec97083d64aa798630ec2e028a
qart <thissak@gmail.com>
2024-03-23 20:46:14 +0900

    add OnFramebufferSizeChange add glad를 활용한 OpenGL 함수 로딩 add glVersion add glfwSetFramebufferSizeCallback

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <spdlog/spdlog.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+void OnFramebufferSizeChange(GLFWwindow* window, int width, int height){
+    SPDLOG_INFO("framebuffer size changed: ({} x {}", width, height);
+    glViewport(0, 0, width, height);
+}
 
 int main(int arc, const char** argv){
     SPDLOG_INFO("Start Program");
@@ -13,6 +19,10 @@ int main(int arc, const char** argv){
         return -1;
     }
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
    // glfw 윈도우 생성, 실패하면 에러 출력후 종료
     SPDLOG_INFO("Create glfw window");
     GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME,
@@ -22,6 +32,19 @@ int main(int arc, const char** argv){
         glfwTerminate();
         return -1;
     }
+    glfwMakeContextCurrent(window);
+
+    // glad를 활용한 OpenGL 함수 로딩
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        SPDLOG_ERROR("failed to initialize glad");
+        glfwTerminate();
+        return -1;
+    }
+    // auto glVersion = glGetString(GL_VERSION);
+    const char* glVersion = reinterpret_cast<const char*>(glGetString(GL_VERSION));
+    SPDLOG_INFO("OpenGL context version: {}", glVersion);
+
+    glfwSetFramebufferSizeCallback(window, OnFramebufferSizeChange);
 
     // glfw 루프실행, 윈도우 CLOSE 버튼을 누르면 정상 종료
     SPDLOG_INFO("Start main loop");
@@ -32,3 +55,4 @@ int main(int arc, const char** argv){
     glfwTerminate();
     return 0;
 }
+
